f46c66e53fc6563ea4bf1d5cd2fe6287c26bd2be
Hasan Mahmood <hasan.mahmood@nordsec.com>
2025-05-27 15:22:54 +0300

    Breaking up Rx into 2 threads
    This commit  optimizes receiving packets in Neptun.
    It achieves this by reading from the socket and writing
    to the tunnel in separate threads and using a channel to
    transfer packets between them.
    Thread 1 -> Reads from socket and decrypts the packet
    Thread 2 -> Writes to the tunnel

--- neptun/src/device/mod.rs
+++ neptun/src/device/mod.rs
@@ -180,6 +180,11 @@ pub struct Device {
 
     tunnel_to_socket_rx: Receiver<NetworkTaskData>,
     tunnel_to_socket_tx: Sender<NetworkTaskData>,
+
+    // UDP socket -> processing -> socket_to_tunnel_tx ->
+    // [thread boundary] -> socket_to_tunnel_rx -> -> write to tunnel
+    socket_to_tunnel_rx: Receiver<TunnelWorkerData>,
+    socket_to_tunnel_tx: Sender<TunnelWorkerData>,
 }
 
 struct ThreadData {
@@ -194,6 +199,32 @@ struct NetworkTaskData {
     buf_len: usize,
     peer: Arc<Peer>,
     iface: Arc<TunSocket>,
+}
+
+struct TunnelWorkerData {
+    buffer: [u8; MAX_PKT_SIZE],
+    peer: Arc<Peer>,
+    iface: Arc<TunSocket>,
+    udp: Arc<socket2::Socket>,
+    res: DecryptResult,
+}
+
+pub enum DecryptResult {
+    Done,
+    Err(WireGuardError),
+    WriteToNetwork(usize),
+    WriteToTunnel(usize, IpAddr),
+}
+
+impl From<TunnResult<'_>> for DecryptResult {
+    fn from(res: TunnResult) -> DecryptResult {
+        match res {
+            TunnResult::Err(err) => Self::Err(err),
+            TunnResult::Done => Self::Done,
+            TunnResult::WriteToNetwork(buffer) => Self::WriteToNetwork(buffer.len()),
+            TunnResult::WriteToTunnel(buffer, addr) => Self::WriteToTunnel(buffer.len(), addr),
+        }
+    }
 }
 
 #[cfg(not(any(target_os = "macos", target_os = "ios", target_os = "tvos")))]
@@ -604,6 +635,7 @@ impl Device {
         let iface = Arc::new(tun.set_non_blocking()?);
         let mtu = iface.mtu()?;
         let (tunnel_to_socket_tx, tunnel_to_socket_rx) = crossbeam_channel::bounded(CHANNEL_SIZE);
+        let (socket_to_tunnel_tx, socket_to_tunnel_rx) = crossbeam_channel::bounded(CHANNEL_SIZE);
         let (close_network_worker_tx, close_network_worker_rx) =
             crossbeam_channel::bounded(num_cpus::get_physical());
 
@@ -630,6 +662,8 @@ impl Device {
             tunnel_to_socket_rx,
             close_network_worker_tx,
             close_network_worker_rx,
+            socket_to_tunnel_tx,
+            socket_to_tunnel_rx,
             update_seq: 0,
         };
 
@@ -722,6 +756,10 @@ impl Device {
                 network_worker(rx_clone, close_chan_clone, udp4_c, udp6_c, fw_callback)
             });
         }
+
+        let rx_clone = self.socket_to_tunnel_rx.clone();
+        let fw_callback = self.config.firewall_process_inbound_callback.clone();
+        thread::spawn(move || tunnel_worker(rx_clone, fw_callback));
 
         self.listen_port = port;
 
@@ -1011,7 +1049,7 @@ impl Device {
                     if d.config.use_connected_socket {
                         // No need for aditional checking, as from this point all packets will arive to connected socket handler
                         if let Ok(sock) = peer.connect_endpoint(d.listen_port, d.config.skt_buffer_size) {
-                            d.register_conn_handler(Arc::clone(peer), sock, ip_addr)
+                            d.register_conn_handler(Arc::clone(peer), Arc::new(sock), ip_addr)
                                 .unwrap();
                         }
                     }
@@ -1030,7 +1068,7 @@ impl Device {
     fn register_conn_handler(
         &self,
         peer: Arc<Peer>,
-        udp: socket2::Socket,
+        udp: Arc<socket2::Socket>,
         peer_addr: IpAddr,
     ) -> Result<(), Error> {
         self.queue.new_event(
@@ -1039,83 +1077,36 @@ impl Device {
                 // The conn_handler handles packet received from a connected UDP socket, associated
                 // with a known peer, this saves us the hustle of finding the right peer. If another
                 // peer gets the same ip, it will be ignored until the socket does not expire.
-                let mut iter = MAX_ITR;
 
-                // Safety: the `recv_from` implementation promises not to write uninitialised
-                // bytes to the buffer, so this casting is safe.
-                let src_buf =
-                    unsafe { &mut *(&mut t.src_buf[..] as *mut [u8] as *mut [MaybeUninit<u8>]) };
-
-                while let Ok(read_bytes) = udp.recv(src_buf) {
-                    let mut flush = false;
-
-                    let res = {
-                        let mut tun = peer.tunnel.lock();
-                        tun.decapsulate(
-                            Some(peer_addr),
-                            &t.src_buf[..read_bytes],
-                            &mut t.dst_buf[..],
-                        )
+                loop {
+                    // Safety: the `recv_from` implementation promises not to write uninitialised
+                    // bytes to the buffer, so this casting is safe.
+                    let src_buf = unsafe {
+                        &mut *(&mut t.src_buf[..] as *mut [u8] as *mut [MaybeUninit<u8>])
                     };
 
-                    match res {
-                        TunnResult::Done => {}
-                        TunnResult::Err(e) => match e {
-                            WireGuardError::DuplicateCounter => {
-                                // TODO(LLT-6071): revert back to having error level for all error types
-                                tracing::debug!(message="Decapsulate error",
-                                    error=?e,
-                                    public_key=peer.public_key.1)
-                            }
-                            _ => {
-                                tracing::error!(message="Decapsulate error",
-                                    error=?e,
-                                    public_key = peer.public_key.1)
-                            }
-                        },
-                        TunnResult::WriteToNetwork(packet) => {
-                            flush = true;
-                            if let Err(err) = udp.send(packet) {
-                                tracing::warn!(message="Failed to write packet", error = ?err);
-                            }
-                        }
-                        TunnResult::WriteToTunnel(packet, addr) => {
-                            if let Some(callback) = &d.config.firewall_process_inbound_callback {
-                                if !callback(&peer.public_key.0, packet) {
-                                    continue;
-                                }
-                            }
-                            if peer.is_allowed_ip(addr) {
-                                _ = t.iface.as_ref().write(packet);
-                                tracing::trace!(
-                                    message = "Writing packet to tunnel",
-                                    interface = ?t.iface.name(),
-                                    packet_length = packet.len(),
-                                    src_addr = ?addr,
-                                    public_key = peer.public_key.1
-                                );
-                            }
-                        }
-                    };
+                    if let Ok(read_bytes) = udp.recv(src_buf) {
+                        let res = {
+                            let mut tun = peer.tunnel.lock();
+                            tun.decapsulate(
+                                Some(peer_addr),
+                                t.src_buf[..read_bytes].as_ref(),
+                                &mut t.dst_buf[..],
+                            )
+                        };
 
-                    if flush {
-                        // Flush pending queue
-                        loop {
-                            let res = {
-                                let mut tun = peer.tunnel.lock();
-                                tun.decapsulate(None, &[], &mut t.dst_buf[..])
-                            };
-                            let TunnResult::WriteToNetwork(packet) = res else {
-                                break;
-                            };
-                            if let Err(err) = udp.send(packet) {
-                                tracing::warn!(message="Failed to flush queue", error = ?err);
-                            }
+                        let worker_data = TunnelWorkerData {
+                            res: res.into(),
+                            buffer: t.dst_buf,
+                            iface: t.iface.clone(),
+                            peer: peer.clone(),
+                            udp: udp.clone(),
+                        };
+                        if let Err(e) = d.socket_to_tunnel_tx.send(worker_data) {
+                            tracing::warn!("Unable to forward data onto tunnel worker {e}");
                         }
-                    }
-
-                    iter -= 1;
-                    if iter == 0 {
+                    } else {
+                        // If the queue is empty break out of the loop
                         break;
                     }
                 }
@@ -1274,6 +1265,74 @@ fn network_worker(
             }
             recv(close_chan) -> _n => {
                 break;
+            }
+        }
+    }
+}
+
+fn tunnel_worker(
+    tunnel_rx: Receiver<TunnelWorkerData>,
+    firewall_process_inbound_callback: Option<Arc<dyn Fn(&[u8; 32], &[u8]) -> bool + Send + Sync>>,
+) {
+    loop {
+        if let Ok(t) = tunnel_rx.recv() {
+            let peer = t.peer;
+            let mut flush = false;
+
+            match t.res {
+                DecryptResult::Done => {}
+                DecryptResult::Err(e) => match e {
+                    WireGuardError::DuplicateCounter => {
+                        // TODO(LLT-6071): revert back to having error level for all error types
+                        tracing::debug!(message="Decapsulate error",
+                            error=?e,
+                            public_key=peer.public_key.1)
+                    }
+                    _ => {
+                        tracing::error!(message="Decapsulate error",
+                            error=?e,
+                            public_key = peer.public_key.1)
+                    }
+                },
+                DecryptResult::WriteToNetwork(len) => {
+                    flush = true;
+                    if let Err(err) = t.udp.send(&t.buffer[..len]) {
+                        tracing::warn!(message="Failed to write packet", error = ?err);
+                    }
+                }
+                DecryptResult::WriteToTunnel(len, addr) => {
+                    if let Some(callback) = &firewall_process_inbound_callback {
+                        if !callback(&peer.public_key.0, &t.buffer[..len]) {
+                            continue;
+                        }
+                    }
+                    if peer.is_allowed_ip(addr) {
+                        _ = t.iface.as_ref().write(&t.buffer[..len]);
+                        tracing::trace!(
+                            message = "Writing packet to tunnel",
+                            packet_length = len,
+                            src_addr = ?addr,
+                            public_key = peer.public_key.1
+                        );
+                    }
+                }
+            };
+
+            if flush {
+                // Flush pending queue
+                loop {
+                    let mut dst_buf = [0u8; MAX_PKT_SIZE];
+                    let res = {
+                        let mut tun = peer.tunnel.lock();
+                        tun.decapsulate(None, &[], &mut dst_buf[..])
+                    };
+                    let TunnResult::WriteToNetwork(packet) = res else {
+                        break;
+                    };
+                    if let Err(err) = t.udp.send(packet) {
+                        tracing::warn!(message="Failed to flush queue", error = ?err);
+                    }
+                }
             }
         }
     }
